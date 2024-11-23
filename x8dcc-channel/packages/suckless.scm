cd93e8908d62f87f418e5b6f3f14e23e68d1c928
8dcc <8dcc.git@gmail.com>
2024-11-23 23:19:14 +0100

    suckless: Indent file
    Change prefix from 'licenses:' to 'license:'.

--- x8dcc-channel/packages/suckless.scm
+++ x8dcc-channel/packages/suckless.scm
@@ -4,7 +4,7 @@
   #:use-module (guix git-download)
   #:use-module (guix build-system gnu)
   #:use-module (guix build utils)
-  #:use-module ((guix licenses) #:prefix licenses:)
+  #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages xorg)
   #:use-module (gnu packages fontutils)
   #:use-module (gnu packages ncurses)
@@ -13,147 +13,147 @@
 
 (define-public dwm
   (package
-   (name "dwm")
-   (version "6.2.0")
-   (synopsis "8dcc's fork of suckless' dynamic window manager")
-   (description "DWM is a dynamic window manager for X. It manages windows in
+    (name "dwm")
+    (version "6.2.0")
+    (synopsis "8dcc's fork of suckless' dynamic window manager")
+    (description "DWM is a dynamic window manager for X. It manages windows in
 tiled, monocle and floating layouts. All of the layouts can be applied
 dynamically, optimising the environment for the application in use and the task
 performed.")
-   (home-page "https://dwm.suckless.org/")
-   (license licenses:x11)
-   (source
-    (origin
-     (method git-fetch)
-     (uri (git-reference
-           (url "https://github.com/8dcc/linux-dotfiles.git")
-           (commit "9808bdb55261ff31f0208f9ae4180d755169bd8d")))
-     (sha256 (base32 "1wnqfw8p7irz9jm3ic6v2dpkywwgvn986l9na1y1vfxvqqrll6p0"))))
-   (build-system gnu-build-system)
-   (arguments
-    `(#:make-flags
-      (list (string-append "CC=" ,(cc-for-target))
-            (string-append "PREFIX=" %output))
-      #:phases
-      (modify-phases %standard-phases
-                     (delete 'configure)
-                     (add-after 'unpack 'change-dir
-                                (lambda _
-                                  (chdir "apps/DWM-6.2/"))))
-      #:tests? #f))
-   (inputs (list pkg-config
-                 xorg-server
-                 xinit
-                 libx11
-                 libxft
-                 libxinerama
-                 freetype))))
+    (home-page "https://dwm.suckless.org/")
+    (license license:x11)
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/8dcc/linux-dotfiles.git")
+             (commit "9808bdb55261ff31f0208f9ae4180d755169bd8d")))
+       (sha256 (base32 "1wnqfw8p7irz9jm3ic6v2dpkywwgvn986l9na1y1vfxvqqrll6p0"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:make-flags
+       (list (string-append "CC=" ,(cc-for-target))
+             (string-append "PREFIX=" %output))
+       #:phases
+       (modify-phases %standard-phases
+         (delete 'configure)
+         (add-after 'unpack 'change-dir
+           (lambda _
+             (chdir "apps/DWM-6.2/"))))
+       #:tests? #f))
+    (inputs (list pkg-config
+                  xorg-server
+                  xinit
+                  libx11
+                  libxft
+                  libxinerama
+                  freetype))))
 
 (define-public st
   (package
-   (name "st")
-   (version "0.8.2.0")
-   (synopsis "8dcc's fork of suckless' simple terminal")
-   (description "St implements a simple and lightweight terminal emulator. It
+    (name "st")
+    (version "0.8.2.0")
+    (synopsis "8dcc's fork of suckless' simple terminal")
+    (description "St implements a simple and lightweight terminal emulator. It
 implements 256 colors, most VT10X escape sequences, utf8, X11 copy/paste,
 antialiased fonts (using fontconfig), fallback fonts, resizing, and line
 drawing.")
-   (home-page "https://st.suckless.org/")
-   (license (list licenses:x11 licenses:expat))
-   (source
-    (origin
-     (method git-fetch)
-     (uri (git-reference
-           (url "https://github.com/8dcc/linux-dotfiles.git")
-           (commit "50d8d779ef01e03a73c7361b4220e13aebbb92de")))
-     (sha256 (base32 "0riw50vc0wq25cn4plfqml12357b9s9lzdwg9qh7vcqhig0j9991"))))
-   (build-system gnu-build-system)
-   (arguments
-    `(#:make-flags
-      (list (string-append "CC=" ,(cc-for-target))
-            (string-append "TERMINFO="
-                           (assoc-ref %outputs "out")
-                           "/share/terminfo")
-            (string-append "PREFIX=" %output))
-      #:phases
-      (modify-phases %standard-phases
-                     (delete 'configure)
-                     (add-after 'unpack 'change-dir
-                                (lambda _
-                                  (chdir "apps/ST-0.8.2/"))))
-      #:tests? #f))
-   (inputs (list pkg-config
-                 libx11
-                 libxft
-                 freetype
-                 ;; For the `tic' command, used when installing.
-                 ncurses))))
+    (home-page "https://st.suckless.org/")
+    (license (list license:x11 license:expat))
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/8dcc/linux-dotfiles.git")
+             (commit "50d8d779ef01e03a73c7361b4220e13aebbb92de")))
+       (sha256 (base32 "0riw50vc0wq25cn4plfqml12357b9s9lzdwg9qh7vcqhig0j9991"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:make-flags
+       (list (string-append "CC=" ,(cc-for-target))
+             (string-append "TERMINFO="
+                            (assoc-ref %outputs "out")
+                            "/share/terminfo")
+             (string-append "PREFIX=" %output))
+       #:phases
+       (modify-phases %standard-phases
+         (delete 'configure)
+         (add-after 'unpack 'change-dir
+           (lambda _
+             (chdir "apps/ST-0.8.2/"))))
+       #:tests? #f))
+    (inputs (list pkg-config
+                  libx11
+                  libxft
+                  freetype
+                  ;; For the `tic' command, used when installing.
+                  ncurses))))
 
 (define-public dmenu
   (package
-   (name "dmenu")
-   (version "5.0.0")
-   (synopsis "8dcc's fork of suckless' dmenu")
-   (description "A dynamic menu for X, originally designed for dwm. It manages
+    (name "dmenu")
+    (version "5.0.0")
+    (synopsis "8dcc's fork of suckless' dmenu")
+    (description "A dynamic menu for X, originally designed for dwm. It manages
 large numbers of user-defined menu items efficiently.")
-   (home-page "https://tools.suckless.org/dmenu/")
-   (license (list licenses:x11))
-   (source
-    (origin
-     (method git-fetch)
-     (uri (git-reference
-           (url "https://github.com/8dcc/linux-dotfiles.git")
-           (commit "5eecfda6bf0facc8219ba65814ca77797b59d86f")))
-     (sha256 (base32 "17s0ar8x9zfsgdiflmj37s6wpx53jc8fa1pfjg1a2df1q9pd5dpp"))))
-   (build-system gnu-build-system)
-   (arguments
-    `(#:make-flags
-      (list (string-append "CC=" ,(cc-for-target))
-            (string-append "PREFIX=" %output))
-      #:phases
-      (modify-phases %standard-phases
-                     (delete 'configure)
-                     (add-after 'unpack 'change-dir
-                                (lambda _
-                                  (chdir "apps/DMENU/"))))
-      #:tests? #f))
-   (inputs (list pkg-config
-                 libx11
-                 libxft
-                 libxinerama))))
+    (home-page "https://tools.suckless.org/dmenu/")
+    (license (list license:x11))
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/8dcc/linux-dotfiles.git")
+             (commit "5eecfda6bf0facc8219ba65814ca77797b59d86f")))
+       (sha256 (base32 "17s0ar8x9zfsgdiflmj37s6wpx53jc8fa1pfjg1a2df1q9pd5dpp"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:make-flags
+       (list (string-append "CC=" ,(cc-for-target))
+             (string-append "PREFIX=" %output))
+       #:phases
+       (modify-phases %standard-phases
+         (delete 'configure)
+         (add-after 'unpack 'change-dir
+           (lambda _
+             (chdir "apps/DMENU/"))))
+       #:tests? #f))
+    (inputs (list pkg-config
+                  libx11
+                  libxft
+                  libxinerama))))
 
 (define-public slock
   (package
-   (name "slock")
-   (version "1.4.0")
-   (synopsis "8dcc's fork of suckless' slock")
-   (description
-    "Simple X session lock with trivial feedback on password entry.")
-   (home-page "https://tools.suckless.org/slock/")
-   (license (list licenses:x11))
-   (source
-    (origin
-     (method git-fetch)
-     (uri (git-reference
-           (url "https://github.com/8dcc/linux-dotfiles.git")
-           (commit "453734ca7b8a3b6e740a8dc8c38f5080d35a2cef")))
-     (sha256 (base32 "1siqshz4lkjh3a0x1hraqx2pabsc9v5h5f0c7ajzwdj6yjjmigsy"))))
-   (build-system gnu-build-system)
-   (arguments
-    `(#:make-flags
-      (list (string-append "CC=" ,(cc-for-target))
-            (string-append "PREFIX=" %output)
-            "SLOCKUSER=")
-      #:phases
-      (modify-phases %standard-phases
-                     (delete 'configure)
-                     (add-after 'unpack 'change-dir
-                                (lambda _
-                                  (chdir "apps/SLOCK/"))))
-      #:tests? #f))
-   (inputs (list pkg-config
-                 libx11
-                 libxft
-                 libxrandr
-                 libxinerama
-                 libxcrypt))))
+    (name "slock")
+    (version "1.4.0")
+    (synopsis "8dcc's fork of suckless' slock")
+    (description
+     "Simple X session lock with trivial feedback on password entry.")
+    (home-page "https://tools.suckless.org/slock/")
+    (license (list license:x11))
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/8dcc/linux-dotfiles.git")
+             (commit "453734ca7b8a3b6e740a8dc8c38f5080d35a2cef")))
+       (sha256 (base32 "1siqshz4lkjh3a0x1hraqx2pabsc9v5h5f0c7ajzwdj6yjjmigsy"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:make-flags
+       (list (string-append "CC=" ,(cc-for-target))
+             (string-append "PREFIX=" %output)
+             "SLOCKUSER=")
+       #:phases
+       (modify-phases %standard-phases
+         (delete 'configure)
+         (add-after 'unpack 'change-dir
+           (lambda _
+             (chdir "apps/SLOCK/"))))
+       #:tests? #f))
+    (inputs (list pkg-config
+                  libx11
+                  libxft
+                  libxrandr
+                  libxinerama
+                  libxcrypt))))
