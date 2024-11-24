cdc9b196c7b73c0f168a9f7db372a61b191d4759
8dcc <8dcc.git@gmail.com>
2024-11-24 12:23:56 +0100

    self: Add 'plumber' package

--- x8dcc-channel/packages/self.scm
+++ x8dcc-channel/packages/self.scm
@@ -32,3 +32,29 @@
        (modify-phases %standard-phases
          (delete 'configure))
        #:tests? #f))))
+
+(define-public plumber
+  (package
+    (name "plumber")
+    (version "1.0.0")
+    (synopsis "Simple alternative to Plan9's plumber.")
+    (description "Run different commands depending on the text format.")
+    (home-page "https://github.com/8dcc/plumber")
+    (license license:gpl3+)
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/8dcc/plumber.git")
+             (commit "v1.0.0")))
+       (sha256
+        (base32 "17x0v8lknnjzjnjrh0kw7pinfw2i6c3b1hv8a0xmvl4p0v6ms44s"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:make-flags
+       (list (string-append "CC=" ,(cc-for-target))
+             (string-append "PREFIX=" %output))
+       #:phases
+       (modify-phases %standard-phases
+         (delete 'configure))
+       #:tests? #f))))
