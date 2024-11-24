b7a78abc1a3cbd988cb5717c142f29c26f487abf
8dcc <8dcc.git@gmail.com>
2024-11-24 12:50:00 +0100

    self: Add 'bin-graph' package

--- x8dcc-channel/packages/self.scm
+++ x8dcc-channel/packages/self.scm
@@ -5,6 +5,8 @@
   #:use-module (guix build-system gnu)
   #:use-module (guix build utils)
   #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages image)
   #:use-module (gnu packages pkg-config))
 
 (define-public snc
@@ -37,7 +39,7 @@
   (package
     (name "plumber")
     (version "1.0.0")
-    (synopsis "Simple alternative to Plan9's plumber.")
+    (synopsis "Simple alternative to Plan9's plumber")
     (description "Run different commands depending on the text format.")
     (home-page "https://github.com/8dcc/plumber")
     (license license:gpl3+)
@@ -58,3 +60,33 @@
        (modify-phases %standard-phases
          (delete 'configure))
        #:tests? #f))))
+
+(define-public bin-graph
+  (package
+    (name "bin-graph")
+    (version "1.0.0")
+    (synopsis "Visualize binary files")
+    (description "This program provides a simple way of visualizing the
+different regions of a binary file.")
+    (home-page "https://github.com/8dcc/bin-graph")
+    (license license:gpl3+)
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/8dcc/bin-graph.git")
+             (commit "45b89b862e1ec445eb099cefecceaaae83840ba2")))
+       (sha256
+        (base32 "16hwj4pr0yppgfaa0hybcknwg63x8bbg2shknk6dknvvhqccddpd"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:make-flags
+       (list (string-append "CC=" ,(cc-for-target))
+             (string-append "PREFIX=" %output))
+       #:phases
+       (modify-phases %standard-phases
+         (delete 'configure))
+       #:tests? #f))
+    (inputs (list libpng
+                  ;; For 'bin-graph-section.sh'.
+                  grep))))
