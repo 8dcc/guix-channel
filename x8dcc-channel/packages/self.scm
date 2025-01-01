bac9a6cb208c6e3db2e87ddb93ba1fb59462e911
8dcc <8dcc.git@gmail.com>
2025-01-01 11:12:59 +0100

    self: Update 'snc' package
    Another Makefile error, it was treating the destination directory as a
    file. Fixed in aefb3077ef57ac845b75f7b8996e65ea1d247d88.

--- x8dcc-channel/packages/self.scm
+++ x8dcc-channel/packages/self.scm
@@ -24,7 +24,7 @@
              (url "https://github.com/8dcc/snc")
              (commit (string-append "v" version))))
        (sha256
-        (base32 "1qvdkgrggakgiz0w5gk0h7k3vy6cb3j8lrbrf5ag1z6p9mbcnnjs"))))
+        (base32 "15n69bsvbiklm3wiyy2kdfzclh34ygjc9ivn4cvnczllw435cfy6"))))
     (build-system gnu-build-system)
     (arguments
      `(#:make-flags
