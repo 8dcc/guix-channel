f750bff6db8d08ae31c3cc6d85fd88fa0e03f5cc
8dcc <8dcc.git@gmail.com>
2026-05-19 21:50:45 +0200

    editor: Add tab width patch for Emacs 29.4
    Since, by default, it reads the "tw" terminal capability, but the
    standard defined by terminfo(5) is "it".

--- x8dcc-channel/packages/editor.scm
+++ x8dcc-channel/packages/editor.scm
@@ -2,6 +2,7 @@
   #:use-module (guix packages)
   #:use-module (guix utils)
   #:use-module (guix download)
+  #:use-module (guix gexp)
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system glib-or-gtk)
   #:use-module ((guix licenses) #:prefix license:)
@@ -37,7 +38,9 @@ every variant; the actual display backend is chosen by the children.")
        (method url-fetch)
        (uri (string-append "mirror://gnu/emacs/emacs-" version ".tar.xz"))
        (sha256
-        (base32 "0dd2mh6maa7dc5f49qdzj7bi4hda4wfm1cvvgq560djcz537k2ds"))))
+        (base32 "0dd2mh6maa7dc5f49qdzj7bi4hda4wfm1cvvgq560djcz537k2ds"))
+       (patches
+        (list (local-file "patches/emacs-29.4-terminfo-tab-width.patch")))))
     (build-system gnu-build-system)
     (arguments
      `(#:tests? #f
